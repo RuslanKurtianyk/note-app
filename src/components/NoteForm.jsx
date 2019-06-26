fd011449efb553cfe23d12089b3779d6e4af2f2a
RuslanKurtianyk <ruslan.kurtjanyk@gmail.com>
2019-06-27 00:22:43 +0300

    fix view of note g=form

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,37 +1,45 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
+import Box from '@material-ui/core/Box'
 import TextField from '@material-ui/core/TextField'
 import SaveIcon from '@material-ui/icons/Save'
 import Container from '@material-ui/core/Container'
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles(() => ({
-    button: {
-      height: 54,
-      marginLeft: 20,
+    container: {
+        display: "flex",
+        alignItems: "center",
     },
-  }));
+    button: {
+        height: "auto",
+        marginLeft: 20,
+    },
+  }))
+
 export const NoteForm = (props) => {
     const { handleSubmit, handleChange } = props
     const classes = useStyles()
 
     return (
         <form onSubmit={handleSubmit}>
-            <Container>
+            <Container className={classes.container}>
                 <TextField
                     name="text" 
                     variant="outlined"
-                    size="small"
+                    margin="dense"
                     onChange={handleChange}
                 />
-                <Button 
-                className={classes.button}
-                variant="contained" 
-                size="large" 
-                type="submit">
-                    <SaveIcon />
-                    Save
-                </Button>
+                <Box>
+                    <Button 
+                        className={classes.button}
+                        variant="contained"
+                        size="large"
+                        type="submit">
+                            <SaveIcon />
+                            Save
+                    </Button>
+                </Box>
             </Container>
         </form>
     )
