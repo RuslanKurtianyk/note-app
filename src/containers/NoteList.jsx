bb33f399d70f283f32bfa6074745fac592bc2f25
RuslanKurtianyk <ruslan.kurtjanyk@gmail.com>
2019-07-09 00:05:11 +0300

    implement saving store to localStorage

--- src/containers/NoteList.jsx
+++ src/containers/NoteList.jsx
@@ -5,17 +5,17 @@ import { removeNote } from '../actions'
 
 const NoteList = ({ dispatch, notes }) => {
   const onNoteRemove = (index) => {
-    console.log('remove', index)
     dispatch(removeNote(index))
   }
   
   return (
-  <ul>
-    {notes.map(note =>
-      <NoteItem key={note.id} text={note.text} onRemove={onNoteRemove.bind(null, note.id)} />
-    )}
-  </ul>
-)}
+    <ul>
+      {notes.map(note =>
+        <NoteItem key={note.id} text={note.text} onRemove={onNoteRemove.bind(null, note.id)} />
+      )}
+    </ul>
+  )
+}
 
 const mapStateToProps = state => ({
   notes: state.notes
